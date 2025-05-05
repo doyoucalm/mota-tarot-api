493cd12d7e1f3a75ff478f5ebe5a1acf245aa01c
doyoucalm <suryaharyadi@gmail.com>
2025-05-05 13:16:42 +0700

    Update tarot.js

--- api/tarot.js
+++ api/tarot.js
@@ -1,16 +1,17 @@
+import cards from '../../data/cards.json';
+
 export default function handler(req, res) {
   // CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://themota.id'); // replace with your exact domain
+  res.setHeader('Access-Control-Allow-Origin', 'https://themota.id');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   // Handle preflight
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
 
-import cards from '../data/cards.json';
-
-export default function handler(req, res) {
+  // Draw card logic
   const randomIndex = Math.floor(Math.random() * cards.length);
   const card = cards[randomIndex];
   res.status(200).json(card);
