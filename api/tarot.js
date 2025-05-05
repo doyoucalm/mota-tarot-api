afbf60aaca047c9a7432d962d06091c819e0a2a8
doyoucalm <suryaharyadi@gmail.com>
2025-05-05 13:15:22 +0700

    Update tarot.js

--- api/tarot.js
+++ api/tarot.js
@@ -3,6 +3,11 @@ export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'https://themota.id'); // replace with your exact domain
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  // Handle preflight
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
 import cards from '../data/cards.json';
 
 export default function handler(req, res) {
